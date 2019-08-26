ce1d7767695b890112b1bc28f7fa5a86b064a183
hushiming476 <1344183846@qq.com>
2019-08-26 17:16:58 +0800

    提交

---
+++ src/main/java/cn/hsm/HelloGit.java
@@ -0,0 +1,15 @@
+package cn.hsm;
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("你好，git");
+        System.out.println("你好，git");
+        System.out.println("你好，git");
+        System.out.println("你好，git");
+        System.out.println("你好，git");
+        System.out.println("你好，git");
+        System.out.println("你好，git");
+        System.out.println("你好，git");
+    }
+}
